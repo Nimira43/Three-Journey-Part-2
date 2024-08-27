a815cdc5e7941fd8997a8cb66a2577fb375dbdcf
Nimira43 <n.rabone72@aol.com>
2024-08-27 19:52:20 +0100

    Enabled damping on controls.

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import './style.css'
 
 const cursor = {
@@ -25,11 +26,12 @@ scene.add(mesh)
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 
-// Place Orthographic Camera code here - see below
-
 camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
@@ -38,33 +40,13 @@ renderer.setSize(sizes.width, sizes.height)
 
 const clock = new THREE.Clock()
 const tick = () => {
-  
+
   const elapsedTime = clock.getElapsedTime()
 
-  // mesh.rotation.y = elapsedTime
-
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-  camera.position.y = cursor.y * 5
-  camera.lookAt(mesh.position)
+  controls.update()
 
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)
 }
 
 tick()
-
-//=========================================================
-// Orthographic Camera code
-//=========================================================
-// const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(
-//   - 1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   - 1,
-//   0.1,
-//   100
-// )
-// camera.position.x = 2
-// camera.position.y = 2
