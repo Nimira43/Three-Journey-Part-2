bca7d51afa750105489ac116d4e4ef7f814b6e33
Nimira43 <n.rabone72@aol.com>
2024-08-27 19:05:08 +0100

    Added mousemove eventlistener to make a tracking cursor for the canvas area.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,16 @@
 import * as THREE from 'three'
 import './style.css'
 
+const cursor = {
+  x: 0,
+  y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5,
+  cursor.y = event.clientY / sizes.height - 0.5
+})
+
 const canvas = document.querySelector('canvas.webgl')
 const sizes = {
   width: 800,
@@ -17,9 +27,7 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 
 
 // Place Orthographic Camera code here - see below
 
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
@@ -32,7 +40,7 @@ const clock = new THREE.Clock()
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
-  mesh.rotation.y = elapsedTime
+  // mesh.rotation.y = elapsedTime
 
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)
@@ -52,3 +60,5 @@ tick()
 //   0.1,
 //   100
 // )
+// camera.position.x = 2
+// camera.position.y = 2
