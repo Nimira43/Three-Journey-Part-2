bb01bfc44588aafcd105f8c9d43f775d2f3b9f5a
Nimira43 <n.rabone72@aol.com>
2024-11-02 10:08:31 +0000

    Updated starting js file

--- src/script.js
+++ src/script.js
@@ -2,27 +2,18 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import './style.css'
 
-const cursor = {
-  x: 0,
-  y: 0
-}
-
-window.addEventListener('mousemove', (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5,
-  cursor.y = - (event.clientY / sizes.height - 0.5)
-})
-
 const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
 const sizes = {
   width: 800,
   height: 600
 }
-const scene = new THREE.Scene()
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({ color: 0xff4500 })
-)
-scene.add(mesh)
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 
