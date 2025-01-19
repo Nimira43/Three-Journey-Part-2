7a7907896eb7acdf6fb6561fe923481648facbf4
Nimira43 <n.rabone72@aol.com>
2025-01-19 09:57:16 +0000

    Improving readability

--- 09-Debugging-UI/src/main.js
+++ 09-Debugging-UI/src/main.js
@@ -14,9 +14,10 @@ const material = new THREE.MeshBasicMaterial({ color: '#ff4500' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-gui.add(mesh.position, 'x', -3, 3, 0.01)
-gui.add(mesh.position, 'y', -3, 3, 0.01)
-gui.add(mesh.position, 'z', -3, 3, 0.01)
+gui.add(mesh.position, 'x').min(- 3).max(3).step(0.01).name('X Position')
+gui.add(mesh.position, 'y').min(- 3).max(3).step(0.01).name('X Position')
+gui.add(mesh.position, 'z').min(- 3).max(3).step(0.01).name('X Position')
+
 
 
 
