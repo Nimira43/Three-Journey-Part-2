b3f38b7c1cf82333f37aabb57c3292706592aa55
Nimira43 <n.rabone72@aol.com>
2025-04-20 06:15:49 +0100

    Added map: texture to material

--- 10-Textures/src/main.js
+++ 10-Textures/src/main.js
@@ -13,8 +13,9 @@ image.src = '/textures/door/color.jpg'
 
 const canvas = document.querySelector('canvas.canvas')
 const scene = new THREE.Scene()
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff4500 })
+const material = new THREE.MeshBasicMaterial({ map: texture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
